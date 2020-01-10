74e9aa05d2b0309fc73cad50e3bb6f594eb34a6c
Pepe Becker <mail@pepebecker.com>
2020-01-10 14:19:44 +0900

    Create tests

---
+++ tests/firestream.spec.ts
@@ -0,0 +1,75 @@
+import 'mocha'
+
+import { initializeApp } from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+import { firebaseConfig } from './firebase-config'
+import { Fire } from '..'
+import { User } from '../dist/chat/user'
+import { ConnectionEventType } from '../dist/events/connection-event'
+import { EventType } from '../dist/events/event-type'
+import { ContactType } from '../dist/types/contact-type'
+
+const app = initializeApp(firebaseConfig)
+Fire.Stream.initialize(app)
+
+const connect = async () => {
+    await app.auth().signInWithEmailAndPassword('node@mail.com', 'pass1234')
+    return new Promise(resolve => {
+        Fire.Stream.getConnectionEvents().subscribe(event => {
+            if (event.getType() === ConnectionEventType.DidConnect) {
+                resolve()
+            }
+        })
+    })
+}
+
+const getTestUser = () => {
+    return new User('t2MwmuyrDGXV2gQyOo54P0Djdsn2')
+}
+
+describe('perform tests', function() {
+    this.timeout(10000)
+    const testUser = getTestUser()
+    const connected = connect()
+
+    it('add contact', async () => {
+        await connected
+        await Fire.Stream.addContact(testUser, ContactType.contact())
+
+        const contacts = Fire.Stream.getContacts()
+        if (contacts.length !== 1) {
+            throw new Error('contacts size must be 1')
+        } else if (!contacts[0].equals(testUser)) {
+            throw new Error('correct user not added to contacts')
+        }
+    })
+
+    it('get contact added', async () => {
+        await connected
+        return new Promise((resolve, reject) => {
+            Fire.Stream.getContactEvents().allEvents().subscribe(event => {
+                if (event.type === EventType.Added) {
+                    if (event.user.equals(testUser)) {
+                        resolve()
+                    } else {
+                        reject(new Error('wrong user added'))
+                    }
+                } else {
+                    reject(new Error('no contact added'))
+                }
+            })
+        })
+    })
+
+    it('delete contact', async () => {
+        await connected
+        await Fire.Stream.removeContact(testUser)
+
+        const contacts = Fire.Stream.getContacts()
+        if (contacts.length !== 0) {
+            throw new Error('contacts size must be 0')
+        }
+    })
+})
